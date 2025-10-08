6701ebe8ab7cce8c832afad56336e5f63171d74f
miiiiiira <miiiiiira>
2025-10-08 14:41:06 +0900

    add class

---
+++ FightForSurvival/Src/Object/SkyDome/SkyDome.cpp
@@ -0,0 +1,29 @@
+#include "SkyDome.h"
+
+SkyDome::SkyDome(void)
+{
+}
+
+SkyDome::~SkyDome(void)
+{
+}
+
+void SkyDome::Load(void)
+{
+}
+
+void SkyDome::Init(void)
+{
+}
+
+void SkyDome::Update(void)
+{
+}
+
+void SkyDome::Draw(void)
+{
+}
+
+void SkyDome::Release(void)
+{
+}
---
+++ FightForSurvival/Src/Object/SkyDome/SkyDome.h
@@ -0,0 +1,20 @@
+#pragma once
+class SkyDome
+{
+public:
+
+	// コンストラクタ
+	SkyDome(void);
+	// デストラクタ
+	~SkyDome(void);
+
+	void Load(void);
+	void Init(void);
+	void Update(void);
+	void Draw(void);
+	void Release(void);
+
+
+private:
+};
+
